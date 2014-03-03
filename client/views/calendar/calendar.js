d10d668c4088039b98ef0e79e8d4e4ef009dcdf0
Dominik Schlaepfer <doemsche@gmx.ch>
2014-03-03 20:02:48 +0100

    make calendar interactive

--- client/views/calendar/calendar.js
+++ client/views/calendar/calendar.js
@@ -4,6 +4,30 @@ $('#calendar').fullCalendar({
 		monthNames: ['Janaur','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
 		monthNamesShort: ['Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
 		dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
-		dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa']
+		dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+		events: function(){
+			var events = Events.find().fetch(),
+				arr = [];
+			_.each(events, function(obj,i,context){
+				var fcObj ={
+					id: obj._id,
+					title: obj.title,
+					start: obj.date,
+					className: 'mEvent-calView',
+					editable: false,
+					backgroundColor: '#ff0000'
+				};
+				console.log(fcObj);
+				arr.push(fcObj);
+			});
+			console.log(arr);
+			return arr;
+		}(),
+		eventClick:function(calEvent,jsEvent,view){
+			// Session.set('editing_calevent',calEvent.id);
+			// Session.set('showEditEvent',true);
+			var eventId = calEvent._id;
+			Router.go('/events/'+eventId);
+		}
 	});
 }
